0b23a491eef295afe6b42d452c9794f78ccccbd4
yoon-chaejin <kevin3st@naver.com>
2022-05-06 14:07:31 +0900

    ✅ file_test 오류 수정 #4

--- test/file_test.py
+++ test/file_test.py
@@ -2,8 +2,9 @@ import unittest
 from main.file import File
 
 class FileTests(unittest.TestCase):
+    
     def test_FileConstructor(self):
-        sample_filename = "sample.docx"
-        file = File(sample_filename)
+        SAMPLE_FILE_ROUTE = "./resources/sample.docx"
+        file = File(SAMPLE_FILE_ROUTE)
 
-        assert(file.filename, sample_filename)
+        self.assertEqual(file.filename, SAMPLE_FILE_ROUTE.split('/')[-1])
