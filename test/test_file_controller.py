32f55d8b6fba80aa328dca2b4d2dec9483e3f2b9
yoon-chaejin <kevin3st@naver.com>
2023-05-01 10:29:47 +0900

    test : load_release_plan_from_docx_file
    테스트용 파일의 경로 조회 방식 수정
    객체 타입 변경 (ReleasePlan -> ReleasePlanFromDocxFile)

--- test/test_file_controller.py
+++ test/test_file_controller.py
@@ -1,16 +1,18 @@
 import unittest
 import main.file_controller as fc
-from main.release_plan import ReleasePlan
+from main.release_plan import ReleasePlanFromDocxFile
+import os
 
 class TestFileController(unittest.TestCase):
 
     def test_loadReleasePlanFromDocxFile(self):
         ##given
-        file_path = './../resources/sample.docx'
+        root_dir = os.getcwd()
+        file_path = root_dir + '/resources/sample.docx'
         title = '제목DATA'
         request_id = 'CHYYMMDD-XXXXXX'
 
-        file_path_copy = './../resources/sample_copy.docx'
+        file_path_copy = root_dir + '/resources/sample_copy.docx'
         title_copy = '제목DATA_COPY'
 
         # when
@@ -18,7 +20,7 @@ class TestFileController(unittest.TestCase):
         release_plan_copy = fc.load_release_plan_from_docx_file(file_path_copy)
 
         # then
-        self.assertIs(type(release_plan), ReleasePlan)
+        self.assertIs(type(release_plan), ReleasePlanFromDocxFile)
         self.assertEquals(release_plan.title, title)
         self.assertEquals(release_plan.request_id, request_id)
 
