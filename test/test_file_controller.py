39d07415fb407f48ed0a26e37682de0b69b35f75
yoon-chaejin <kevin3st@naver.com>
2023-05-01 10:07:59 +0900

    test : load_release_plan_from_docx_file
    테스트 항목 추가 : 제목이 다른 COPY 파일 추가

--- test/test_file_controller.py
+++ test/test_file_controller.py
@@ -10,13 +10,19 @@ class TestFileController(unittest.TestCase):
         title = '제목DATA'
         request_id = 'CHYYMMDD-XXXXXX'
 
+        file_path_copy = './../resources/sample_copy.docx'
+        title_copy = '제목DATA_COPY'
+
         # when
         release_plan = fc.load_release_plan_from_docx_file(file_path)
+        release_plan_copy = fc.load_release_plan_from_docx_file(file_path_copy)
 
         # then
         self.assertIs(type(release_plan), ReleasePlan)
         self.assertEquals(release_plan.title, title)
         self.assertEquals(release_plan.request_id, request_id)
+
+        self.assertEquals(release_plan_copy.title, title_copy)
     
 
 if __name__ == '__main__':
