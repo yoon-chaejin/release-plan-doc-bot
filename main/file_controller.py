3ab398c2af49c92329a742919133960555731ffe
yoon-chaejin <kevin3st@naver.com>
2023-05-01 10:41:56 +0900

    refactor : file_controller 제거

--- main/file_controller.py
+++
@@ -1,4 +0,0 @@
-from main.release_plan import ReleasePlanFromDocxFile
-
-def load_release_plan_from_docx_file(file_path):
-    return ReleasePlanFromDocxFile(file_path)
--- test/test_release_plan.py
+++ test/test_release_plan.py
@@ -1,11 +1,10 @@
 import unittest
-import main.file_controller as fc
 from main.release_plan import ReleasePlanFromDocxFile
 import os
 
-class TestFileController(unittest.TestCase):
+class TestReleasePlan(unittest.TestCase):
 
-    def test_loadReleasePlanFromDocxFile(self):
+    def test_constructor_ReleasePlanFromDocxFile(self):
         ##given
         root_dir = os.getcwd()
         file_path = root_dir + '/resources/sample.docx'
@@ -16,16 +15,14 @@ class TestFileController(unittest.TestCase):
         title_copy = '제목DATA_COPY'
 
         # when
-        release_plan = fc.load_release_plan_from_docx_file(file_path)
-        release_plan_copy = fc.load_release_plan_from_docx_file(file_path_copy)
+        release_plan = ReleasePlanFromDocxFile(file_path)
+        release_plan_copy = ReleasePlanFromDocxFile(file_path_copy)
 
         # then
-        self.assertIs(type(release_plan), ReleasePlanFromDocxFile)
         self.assertEquals(release_plan.title, title)
         self.assertEquals(release_plan.request_id, request_id)
 
         self.assertEquals(release_plan_copy.title, title_copy)
-    
 
 if __name__ == '__main__':
     unittest.main()
