dfa8745ceab411a50646cc4065ba351e2e7b4f12
yoon-chaejin <kevin3st@naver.com>
2023-05-01 09:55:16 +0900

    test : load_release_plan_from_docx_file
    테스트 항목 추가 : 제목과 SOR_ID 검증

--- test/test_file_controller.py
+++ test/test_file_controller.py
@@ -7,12 +7,16 @@ class TestFileController(unittest.TestCase):
     def test_loadReleasePlanFromDocxFile(self):
         ##given
         file_path = './../resources/sample.docx'
+        title = '제목DATA'
+        request_id = 'CHYYMMDD-XXXXXX'
 
         # when
         release_plan = fc.load_release_plan_from_docx_file(file_path)
 
         # then
         self.assertIs(type(release_plan), ReleasePlan)
+        self.assertEquals(release_plan.title, title)
+        self.assertEquals(release_plan.request_id, request_id)
     
 
 if __name__ == '__main__':
