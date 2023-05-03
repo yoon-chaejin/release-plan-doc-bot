436694ec20392be60c7cb3b9d80e558ff2786c78
yoon-chaejin <kevin3st@naver.com>
2023-05-03 23:48:51 +0900

    code : create_daily_report (#14)

--- main/main.py
+++ main/main.py
@@ -1,4 +1,10 @@
 from docx import Document
 
 def create_daily_report(release_plans):
-    return Document()
+    daily_report = Document()
+
+    daily_report.add_table(3,2)
+    daily_report.tables[0].cell(0,0).text = "작업자"
+    daily_report.tables[0].cell(0,1).text = "작업 내용"
+
+    return daily_report
